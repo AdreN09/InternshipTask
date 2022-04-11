b0c470ca9d3546264dca899c7ce176e692312589
Adrian Krawczyk <adrian.krawczyk@icloud.com>
2022-04-11 21:37:49 +0200

    added protection against entering wrong number of arguments

--- InternshipTask.py
+++ InternshipTask.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # file_path = 'D:\Python\Data\pokemon.csv'
 # join C:\InternshipTask\Pokemon1.csv
-# join C:\InternshipTask\Pokemon1.csv C:\InternshipTask\Pokemon2.csv
+# join C:\InternshipTask\Pokemon1.csv C:\InternshipTask\Pokemon2.csv Name
 
 print("Internship Task")
 
@@ -15,22 +15,25 @@ def start():
     command = input("Your command: ")
     if "join" in command:
         command_sp = command.split(" ")
-        if ".csv" in command_sp[1]:
-            file_path1 = pd.read_csv(command_sp[1])
-            if ".csv" in command_sp[2]:
-                file_path2 = pd.read_csv(command_sp[2])
-                if (command_sp[3] in file_path1) & (command_sp[3] in file_path2):
-                    df = pd.concat([file_path1, file_path2])
-                    print(df)
-                    print(df.columns)
+        if len(command_sp) == 4:
+            if ".csv" in command_sp[1]:
+                file_path1 = pd.read_csv(command_sp[1])
+                if ".csv" in command_sp[2]:
+                    file_path2 = pd.read_csv(command_sp[2])
+                    if (command_sp[3] in file_path1) & (command_sp[3] in file_path2):
+                        df = pd.concat([file_path1, file_path2])
+                        print(df)
+                    else:
+                        print("Name of column:  " + command_sp[3] + " doesn't exist in both csv's files!")
+                        start()
                 else:
-                    print("Name of column:  " + command_sp[3] + " doesn't exist in both csv's files!")
+                    print("Argument " + command_sp[2] + " is wrong!")
                     start()
             else:
-                print("Argument " + command_sp[2] + " is wrong!")
+                print("Argument " + command_sp[1] + " is wrong!")
                 start()
         else:
-            print("Argument " + command_sp[1] + " is wrong!")
+            print("Wrong numbers of arguments!")
             start()
     else:
         print("You can't use this command :/")
