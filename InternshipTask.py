eafde1018d244be9c9448b8582d3d374ca8f6ee2
Adrian Krawczyk <adrian.krawczyk@icloud.com>
2022-04-11 21:22:50 +0200

    combining data from files

--- InternshipTask.py
+++ InternshipTask.py
@@ -8,6 +8,7 @@ import pandas as pd
 # join C:\InternshipTask\Pokemon1.csv C:\InternshipTask\Pokemon2.csv
 
 print("Internship Task")
+
 def start():
     print("To execute the program, execute the command: join file_path file_path column_name join_type")
     command = input("Your command: ")
@@ -15,10 +16,12 @@ def start():
         command_sp = command.split(" ")
         if(".csv" in command_sp[1]):
             file_path1 = pd.read_csv(command_sp[1])
-            print(file_path1)
+            # print(file_path1)
             if(".csv" in command_sp[2]):
                 file_path2 = pd.read_csv(command_sp[2])
-                print(file_path2)
+                # print(file_path2)
+                df = pd.concat([file_path1, file_path2])
+                print(df)
             else:
                 print("Argument " + command_sp[2] + " is wrong!")
                 start()
