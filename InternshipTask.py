edc76d1fa6312cffe7252a5972905a6a737b5591
Adrian Krawczyk <adrian.krawczyk@icloud.com>
2022-04-11 21:32:11 +0200

    loading data if the column name appers in both files

--- InternshipTask.py
+++ InternshipTask.py
@@ -9,19 +9,23 @@ import pandas as pd
 
 print("Internship Task")
 
+
 def start():
     print("To execute the program, execute the command: join file_path file_path column_name join_type")
     command = input("Your command: ")
     if "join" in command:
         command_sp = command.split(" ")
-        if(".csv" in command_sp[1]):
+        if ".csv" in command_sp[1]:
             file_path1 = pd.read_csv(command_sp[1])
-            # print(file_path1)
-            if(".csv" in command_sp[2]):
+            if ".csv" in command_sp[2]:
                 file_path2 = pd.read_csv(command_sp[2])
-                # print(file_path2)
-                df = pd.concat([file_path1, file_path2])
-                print(df)
+                if (command_sp[3] in file_path1) & (command_sp[3] in file_path2):
+                    df = pd.concat([file_path1, file_path2])
+                    print(df)
+                    print(df.columns)
+                else:
+                    print("Name of column:  " + command_sp[3] + " doesn't exist in both csv's files!")
+                    start()
             else:
                 print("Argument " + command_sp[2] + " is wrong!")
                 start()
@@ -30,5 +34,6 @@ def start():
             start()
     else:
         print("You can't use this command :/")
+
 
 start()
