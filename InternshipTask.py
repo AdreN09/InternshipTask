788f668d92ad7171ea43b7e75eb617ee08be7dfa
Adrian Krawczyk <adrian.krawczyk@icloud.com>
2022-04-12 22:48:56 +0200

    code improvement and testing

--- InternshipTask.py
+++ InternshipTask.py
@@ -1,5 +1,5 @@
-# Intership Task
-# Autor: Adrian Krawczyk
+# Internship Task
+# Author: Adrian Krawczyk
 
 import pandas as pd
 
@@ -7,15 +7,16 @@ import pandas as pd
 # Pokemon1.csv
 # Pokemon2.csv
 # examples for using:
-# join C:\InternshipTask\Pokemon1.csv
 # join C:\InternshipTask\Pokemon1.csv C:\InternshipTask\Pokemon2.csv Name
-# join C:\InternshipTask\Pokemon1.csv C:\InternshipTask\Pokemon2.csv Product_ID inner
 
 # testing data for 5 arguments
 # Customers.csv
 # Products.csv
 # examples for using:
+# join C:\InternshipTask\Products.csv C:\InternshipTask\Customers.csv Product_ID
 # join C:\InternshipTask\Products.csv C:\InternshipTask\Customers.csv Product_ID inner
+# join C:\InternshipTask\Products.csv C:\InternshipTask\Customers.csv Product_ID left
+# join C:\InternshipTask\Products.csv C:\InternshipTask\Customers.csv Product_ID right
 
 
 print("Internship Task")
@@ -33,19 +34,18 @@ def read():
                 df.to_csv("C:\InternshipTask\dataFrame.csv")
                 print(df)
             else:
-                print("Name of column:  " + command_sp[3] + " doesn't exist in both csv's files! Try again.")
+                print("Name of column: '" + command_sp[3] + "' doesn't exist in both csv's files! Try again.")
         else:
-            print("Argument " + command_sp[2] + " is wrong! Try again.")
+            print("Argument '" + command_sp[2] + "' is wrong! Try again.")
     else:
-        print("Argument " + command_sp[1] + " is wrong! Try again.")
+        print("Argument '" + command_sp[1] + "' is wrong! Try again.")
 
-def type_join():
+def join_type():
     if ".csv" in command_sp[1]:
         file_path1 = pd.read_csv(command_sp[1])
         if ".csv" in command_sp[2]:
             file_path2 = pd.read_csv(command_sp[2])
             if (command_sp[3] in file_path1) & (command_sp[3] in file_path2):
-                # df = pd.concat([file_path1, file_path2])
                 if "inner" in command_sp[4]:
                     df_inner = pd.merge(file_path1, file_path2, on=command_sp[3])
                     df_inner.to_csv("C:\InternshipTask\dataFrame.csv")
@@ -59,32 +59,23 @@ def type_join():
                     df_right.to_csv("C:\InternshipTask\dataFrame.csv")
                     print(df_right)
                 else:
-                    print("Argument " + command_sp[4] + " is wrong!")
-                    main()
-                # df.to_csv("C:\InternshipTask\dataFrame.csv")
-                # print(df)
+                    print("Argument '" + command_sp[4] + "' is wrong! Try again.")
             else:
-                print("Name of column:  " + command_sp[3] + " doesn't exist in both csv's files!")
-                main()
+                print("Name of column:  '" + command_sp[3] + "' doesn't exist in both csv files! Try again.")
         else:
-            print("Argument " + command_sp[2] + " is wrong!")
-            main()
+            print("Argument '" + command_sp[2] + "' is wrong! Try again.")
     else:
-        print("Argument " + command_sp[1] + " is wrong!")
-        main()
-
+        print("Argument '" + command_sp[1] + "' is wrong! Try again.")
 
 def main():
     if "join" in command:
         if len(command_sp) == 4:
             read()
         elif len(command_sp) == 5:
-            type_join()
+            join_type()
         else:
             print("Wrong numbers of arguments! Try again.")
     else:
         print("You can't use this command :/")
 
 main()
-
-# print(pd.read_csv(command_sp[1]))
